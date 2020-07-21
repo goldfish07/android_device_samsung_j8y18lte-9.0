00382e8a8df207df630deacff2c626881f3b3c74
Ayush <ayushbisht5663@gmail.com>
2020-07-21 07:59:32 +0530

    j8y18lte: set build fingerprints props

--- lineage.mk
+++ lineage.mk
@@ -13,3 +13,10 @@ PRODUCT_NAME := lineage_j8y18lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := j8y18lte
 PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="j8y18ltedd-user 8.0.0 R16NW J810GDDU2ASB1 release-keys"
+#BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := samsung/j8y18ltedd/j8y18lte:8.0.0/R16NW/J810GDDU2ASB1:user/release-keys
+
+
