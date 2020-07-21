d1fb984f936220d65c6505631cc6cdbec0c979e3
Ayush <ayushbisht5663@gmail.com>
2020-07-21 09:43:32 +0530

    j8y18lte: RenderScript hals

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -228,6 +228,9 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
