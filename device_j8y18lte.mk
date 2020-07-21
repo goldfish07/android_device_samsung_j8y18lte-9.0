d95bd64401387542f430151c0e0f85cd7b12ea03
Ayush <ayushbisht5663@gmail.com>
2020-07-21 08:44:40 +0530

    j8y18lte: update bluetooth configs

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -98,6 +98,14 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     tinymix
 
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
+    libbt-vendor
+
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
