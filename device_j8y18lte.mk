f5d5c9a6f607a758b8a615f61c14b4da9cf9a274
Ayush <ayushbisht5663@gmail.com>
2020-07-21 10:20:44 +0530

    j8y18lte: basic hild

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -320,6 +320,11 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
     SystemUI \
     framework-res \
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
