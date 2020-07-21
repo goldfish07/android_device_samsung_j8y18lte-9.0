5f6053fa5e4e632990cc1e368f6a89001121f6a3
Ayush <ayushbisht5663@gmail.com>
2020-07-21 09:33:08 +0530

    j8y18lte: Vibrator hals

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -230,6 +230,13 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
+
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
