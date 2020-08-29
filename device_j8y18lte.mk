35386e988c35287d6beb8efb349a80752fad1c98
Ayush <ayushbisht5663@gmail.com>
2020-08-29 13:43:44 +0530

    j8y18lte: add audio.primary dependencies

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -147,7 +147,8 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
-    tinymix
+    tinymix \
+    libaudioroute
 
 # ANT
 PRODUCT_PACKAGES += \
