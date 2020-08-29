bc505c08af2d596596519bb84f179b5ef44db49f
Ayush <ayushbisht5663@gmail.com>
2020-08-29 17:00:56 +0530

    j8y18lte: use samsung gnns HAL

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -236,7 +236,6 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     vendor.samsung.hardware.gnss@1.0-service \
-    android.hardware.gnss@1.0-impl-qti \
     gps.msm8953 \
     libcurl \
     libgnss \
