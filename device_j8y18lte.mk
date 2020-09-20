edfe942602bf01d4866cad46601c71bdc1dd8510
Ayush <ayushbisht5663@gmail.com>
2020-09-20 13:01:03 +0530

    j8y18lte: update bluetooth configuration

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -216,7 +216,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service \
+    javax.btobex \
     libbt-vendor
 
 
