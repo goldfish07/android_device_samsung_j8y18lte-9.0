4b369059e83e1e86862d2115fd53c840c4eab016
Ayush Bisht <ayushbisht5663@gmail.com>
2021-01-16 04:35:53 +0530

    j8y18lte: build gnss qti libarary instead

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -301,7 +301,7 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-impl-qti \
     libcurl \
     libgnss \
     libgnsspps
