c82dbea0bbb50cee3974172bc38ed591bedfcf2f
Ayush Bisht <ayushbisht5663@gmail.com>
2021-03-18 05:59:55 +0530

    j8y18lte: remove useless vendor.display.color@1.0 vndk hal

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -31,7 +31,6 @@ EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
     android.hidl.memory@1.0 \
     android.hidl.manager@1.0 \
-    vendor.display.color@1.0 \
     vendor.display.config@1.0 
 
 
