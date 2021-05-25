d6fb54ef7de00a26d0ecc50bd8142a51ea9a04ca
Ayush Bisht <ayushbisht5663@gmail.com>
2021-05-25 06:13:26 +0530

    j8y18lte: migrate linage.mk to lineage_j8y18lte.mk
    Info it seems that lineage-16.0 can't detect our j8y18lte makefiles , forcing us to rename lineage.mk to lineage_j8y18lte.mk

--- lineage_j8y18lte.mk
+++ lineage_j8y18lte.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := j8y18lte
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/j8y18lte/device_j8y18lte.mk)
+$(call inherit-product, device/samsung/j8y18lte/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j8y18lte
